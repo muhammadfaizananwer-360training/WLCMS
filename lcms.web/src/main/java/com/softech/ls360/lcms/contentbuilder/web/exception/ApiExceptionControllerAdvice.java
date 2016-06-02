81196282ce5d7efc765bd05a850c72dd34265c37
Abdul Wahid <abdul.wahid@360training.com>
2016-06-02 16:31:47 +0500

    WLCMS-2834 Error Codes for WLCMS API (removed not used imports)

--- lcms.web/src/main/java/com/softech/ls360/lcms/contentbuilder/web/exception/ApiExceptionControllerAdvice.java
+++ lcms.web/src/main/java/com/softech/ls360/lcms/contentbuilder/web/exception/ApiExceptionControllerAdvice.java
@@ -1,27 +1,14 @@
 package com.softech.ls360.lcms.contentbuilder.web.exception;
 
-import com.softech.common.validator.InvalidField;
-import com.softech.common.validator.InvalidModelException;
+import com.softech.ls360.lcms.contentbuilder.web.model.RestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import com.softech.ls360.lcms.contentbuilder.web.model.RestResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice(annotations = RestController.class)
 public class ApiExceptionControllerAdvice {
@@ -36,8 +23,6 @@ public class ApiExceptionControllerAdvice {
         RestResponse response = new RestResponse();
         try {
             throw e;
-        } catch (InvalidModelException ex) {
-
         } catch (Exception ex) {
             response.setError(e.toString());
             logger.error("Unhandled API Exception", e);
